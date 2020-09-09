4e450528611effcdcf73235d637b3b308b2bc48d
Gavin Swofford <gavinswofford@gmail.com>
2020-09-08 20:18:57 -0500

    muscle and lockspecialist are comeplete

--- Hacker.cs
+++ Hacker.cs
@@ -6,6 +6,7 @@ namespace HeistPartTwo
     class Hacker : IRobber
     {
         public string Name { get; set; }
+
         public int SkillLevel { get; set; }
 
         public int PercentageCut { get; set; }
